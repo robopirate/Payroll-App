b46b0b1e25772bd36faed31570d926f9bbe9875e
robopirate <info@robopirate.in>
2026-06-08 13:37:45 +0530

    Increase rate limits for school WiFi shared IP

--- extensions.py
+++ extensions.py
@@ -10,6 +10,6 @@ login_manager = LoginManager()
 csrf = CSRFProtect()
 limiter = Limiter(
     get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["5000 per day", "1000 per hour"],
     storage_uri="memory://",
 )
